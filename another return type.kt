cbf8a0f91064e7a1ca4834d5e9875ad78fc9dd8d
Hukum Budha <103075782+OGhukum@users.noreply.github.com>
2024-05-03 07:26:36 +0545

    Create another return type.kt

---
+++ another return type.kt
@@ -0,0 +1,9 @@
+fun main(){
+ //val greet= birthdayGreet()
+ println(birthdayGreet())
+}
+fun birthdayGreet() : String{
+   val nameGreet= "Happy birthday Hukum"
+   val ageGreet= "For 23 years old"
+   return "$nameGreet\n $ageGreet"
+}
