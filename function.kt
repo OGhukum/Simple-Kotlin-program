0e986382be4500cbb86c93677be066369e1bc2c8
Hukum Budha <103075782+OGhukum@users.noreply.github.com>
2024-05-03 07:25:15 +0545

    Create function.kt

---
+++ function.kt
@@ -0,0 +1,9 @@
+fun main(){
+ val greet= birthdayGreet()
+ println(greet)
+}
+fun birthdayGreet() : String{
+   val nameGreet= "Happy birthday Hukum"
+   val ageGreet= "For 23 years old"
+   return "$nameGreet\n $ageGreet"
+}
